aa9dc2c29b7eabdf5df9a97bf83689584349781c
eduard3536 <avetisyaneduard61@gmail.com>
2023-07-27 20:10:06 +0400

    undate 27.07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Main from './Components/Main'
+import Shop from './Components/Shop'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    showComp: false
+  }
+  changeState = () => {
+   this.setState({
+    showComp: true
+  })       
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.state.showComp ? <Shop /> : <Main type={this.changeState} />
+        }
+      
+      </div>
+    )
+  }
 }
-
-export default App;
